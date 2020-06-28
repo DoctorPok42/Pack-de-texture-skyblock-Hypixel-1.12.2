08766c80fbc7fcf9663e4cd9e4b51e0ce0d4c153
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2020-06-28 10:15:26 +0200

    t
    t

--- ------ BotTest ------/commands/Admin/reload.js
+++
@@ -1,9 +0,0 @@
-const { MESSAGES } = require("../../util/constants");
-
-module.exports.run = async (client, message, args) => {
-  await message.delete();
-  await client.channels.cache.get('720977762650816573').send("Le bot redémarre!");
-  process.exit();
-};
-
-module.exports.help = MESSAGES.COMMANDS.ADMIN.RELOAD;
--- ------ BotTest ------/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  TOKEN: "NjkwMTg2ODU0ODc1NjYwMzQ0.XvBz-Q.hxU4kw_QOfN2Xfb57eUrXYhe8DQ",
-  DBCONNECTION: "mongodb+srv://RedGhost:mpol69az@test-bm6ib.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  DEFAULTSETTINGS: {
-    prefix: "?",
-    logChannel: "bot-logs",
-    welcomeMessage: "Welcome {{user}}"
-  }
-}
